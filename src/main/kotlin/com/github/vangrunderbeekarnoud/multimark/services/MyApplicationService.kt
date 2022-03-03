18abc588d781ee4f11507b39a16e847c91581c30
GitHub Action <action@github.com>
2022-03-03 11:41:25 +0000

    Template cleanup

---
+++ src/main/kotlin/com/github/vangrunderbeekarnoud/multimark/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.vangrunderbeekarnoud.multimark.services
+
+import com.github.vangrunderbeekarnoud.multimark.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
